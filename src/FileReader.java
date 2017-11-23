09f5244322d76358868d990fd21494d7d1fe8d6f
Cris <cris.kgl@gmail.com>
2017-11-23 14:19:03 +0100

    algunos cambios sin importancia al file reader

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class FileReader {
 
@@ -5,9 +7,13 @@ public class FileReader {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(new File("input.txt"));
+		
+		System.out.println();	
+		
+		while(input.hasNextInt())
 	}
-
 }
